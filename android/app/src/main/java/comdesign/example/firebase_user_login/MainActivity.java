4d9b8adba4779c5132b16de5031286265a695e80
thoyyibtvt <mtpt.muhammed@gmail.com>
2021-08-21 22:58:05 +0530

    test project commit last 8-21-2021

--- android/app/src/main/java/comdesign/example/firebase_user_login/MainActivity.java
+++ android/app/src/main/java/comdesign/example/firebase_user_login/MainActivity.java
@@ -11,19 +11,7 @@ import android.os.Bundle;
 
 
 
-import android.view.ViewTreeObserver
-import android.app.ActivityManager
-import android.content.Context
-import android.content.pm.ApplicationInfo
-import android.content.pm.PackageManager
 
-import android.util.Log
-import android.view.SurfaceView
-import android.view.View
-import android.view.ViewGroup
-
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugin.common.MethodChannel
 
 
 
@@ -44,59 +32,6 @@ public class MainActivity extends FlutterActivity {
     getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE);
   }
 
-
-
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    val flutter_native_splash = true
-    var originalStatusBarColor = 0
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-      originalStatusBarColor = window.statusBarColor
-      window.statusBarColor = 0xffeaeaea.toInt()
-    }
-    val originalStatusBarColorFinal = originalStatusBarColor
-
-
-    if (!setSecureSurfaceView()) {
-      Log.e("MainActivity", "Could not secure the MainActivity!")
-      // React as appropriate.
-    }
-    getWindow().addFlags(LayoutParams.FLAG_SECURE)
-    getWindow().setFlags(LayoutParams.FLAG_SECURE,
-            LayoutParams.FLAG_SECURE)
-    SurfaceView(applicationContext).setSecure(true)
-  }
-  private fun setSecureSurfaceView(): Boolean {
-    val content = findViewById<ViewGroup>(android.R.id.content)
-    if (!isNonEmptyContainer(content)) {
-      return false
-    }
-    val splashView = content.getChildAt(0)
-    if (!isNonEmptyContainer(splashView)) {
-      return false
-    }
-    val flutterView = (splashView as ViewGroup).getChildAt(0)
-    if (!isNonEmptyContainer(flutterView)) {
-      return false
-    }
-    val surfaceView = (flutterView as ViewGroup).getChildAt(0)
-    if (surfaceView !is SurfaceView) {
-      return false
-    }
-    surfaceView.setSecure(true)
-    this.window.setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE)
-    return true
-  }
-
-  private fun isNonEmptyContainer(view: View): Boolean {
-    if (view !is ViewGroup) {
-      return false
-    }
-    if (view.childCount < 1) {
-      return false
-    }
-    return true
-  }
 
 
 }
